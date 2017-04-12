e9353929060dde1628603d13869ace1b868b8f3a
Michael Aufreiter <michael@substance.io>
2017-04-12 12:09:08 +0200

    Fix Configurator.getSurfaceCommandNames()

--- ui/Configurator.js
+++ ui/Configurator.js
@@ -508,7 +508,7 @@ class Configurator {
   getSurfaceCommandNames() {
     var commands = this.getCommands()
     var commandNames = commands.map(function(C) {
-      return C.type
+      return C.name
     })
     return commandNames
   }
